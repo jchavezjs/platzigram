05592e9726fd8fafebb9b0add993f58da211e89b
Jaime Chavez <jaime-mcr@hotmail.com>
2016-05-19 18:40:27 -0600

    Rutas dinamicas con Page

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function(req, res){
   res.render('index');
 })
 
+app.get('/signup', function(req, res){
+  res.render('index');
+})
+
+app.get('/signin', function(req, res){
+  res.render('index');
+})
+
 app.listen(3000, function(err){
   if (err) return console.log('No se peude inciar el servidor'), process.exit(1);
   console.log('Escuchando el puerto 3000');
